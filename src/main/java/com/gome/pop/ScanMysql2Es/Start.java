4705aceb46aabd62e95ca8eca3b4d8c5135d90b4
sycadidas <sycadidas@163.com>
2017-04-25 16:11:03 +0800

    代码修改

--- src/main/java/com/gome/pop/ScanMysql2Es/Start.java
+++ src/main/java/com/gome/pop/ScanMysql2Es/Start.java
@@ -24,31 +24,7 @@ import com.gome.pop.ScanMysql2Es.base.BaseDAO;
 public class Start 
 {
 
-	static Integer countSql() throws SQLException{
-		String countSql = "SELECT count(so.SUB_ORDER_ID) as totalCount"
-		 		+ " FROM tbl_sub_order so  LEFT JOIN tbl_order_consignee oc ON so.sub_Order_Id = oc.sub_order_id  "
-		 		+ " LEFT JOIN tbl_tracking_no tn ON so.sub_Order_Id = tn.sub_order_id "
-		 		+ " LEFT JOIN (SELECT s.SUB_ORDER_ID, GROUP_CONCAT( DISTINCT s.sku) SKU,GROUP_CONCAT(DISTINCT  s.GOODS_NAME) GOODS_NAME  from tbl_cart_goods s GROUP BY s.SUB_ORDER_ID) aa on so.sub_Order_Id = aa.SUB_ORDER_ID";
-		
-		  @SuppressWarnings("resource")
-		  ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-	      BaseDAO dao=(BaseDAO)context.getBean("baseDAO");
-	      Connection conn = dao.getConnection();
-	      PreparedStatement stmt = conn.prepareStatement(countSql);
-	      ResultSet set=stmt.executeQuery();
-	      
-	      if(set.next()){
-	    	  return set.getInt("totalCount");
-	      }else{
-	    	  return null;
-	      }
-	      
-//	      return set.getInt("totalCount");
-	      
-		
-	}
-	
-	static void creatJsonFromDB(int startIndex, int page_size) throws Exception{
+	static void creatJsonFromDB(int startIndex, int page_size, ApplicationContext context, Client client) throws Exception{
 		 String sql="SELECT so.ORDER_GOODS_TYPE ORDER_GOODS_TYPE, date_format(so.DLRT_TIME, '%Y-%m-%d %h:%i:%s') DLRT_TIME, so.SALE_TAX SALE_TAX,so.VAT_TAX VAT_TAX,so.COMPREHENSIVE_TAX COMPREHENSIVE_TAX,"
 		 		+ "so.order_no ORDER_NO,so.sub_Order_Id SUB_ORDER_ID,so.order_Origin ORDER_ORIGIN,so.SUB_ORDER_TYPE SUB_ORDER_TYPE,so.total_Price TOTAL_PRICE,date_format(so.order_Date, '%Y-%m-%d %h:%i:%s') ORDER_DATE,"
 		 		+ "so.order_State ORDER_STATE,	date_format(so.order_State_Time, '%Y-%m-%d %h:%i:%s') ORDER_STATE_TIME,so.REMARK,so.opinion_desc OPINION_DESC,so.pay_model PAY_MODEL,so.FREIGHT,so.pay_sate PAY_SATE,so.own_shop OWN_SHOP,"
@@ -64,9 +40,7 @@ public class Start
 		 		+ " LEFT JOIN tbl_tracking_no tn ON so.sub_Order_Id = tn.sub_order_id "
 		 		+ " LEFT JOIN (SELECT s.SUB_ORDER_ID, GROUP_CONCAT( DISTINCT s.sku) SKU,GROUP_CONCAT(DISTINCT  s.GOODS_NAME) GOODS_NAME  from tbl_cart_goods s GROUP BY s.SUB_ORDER_ID) aa on so.sub_Order_Id = aa.SUB_ORDER_ID"
 		 		+ " limit "+ startIndex+","+page_size;
-		
-		  @SuppressWarnings("resource")
-		  ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		 
 	      BaseDAO dao=(BaseDAO)context.getBean("baseDAO");
 	      Connection conn = dao.getConnection();
 	      PreparedStatement stmt = conn.prepareStatement(sql);
@@ -76,18 +50,7 @@ public class Start
 	      ResultSetMetaData md = set.getMetaData();
 	      int columnCount = md.getColumnCount();  
 	      
-	  	@SuppressWarnings("unused")
-		Settings settings =  Settings.settingsBuilder().put("cluster.name", "elasticsearch").build();
-		Client client =null;
-		
-		try {
-			client = TransportClient.builder().build()  
-		            .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.58.62.47"), 9300));//10.58.62.47 杨宇集群
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		BulkRequestBuilder bulkRequest = client.prepareBulk();
+		  BulkRequestBuilder bulkRequest = client.prepareBulk();
 		 
 		int ss=0;
 		
@@ -110,7 +73,7 @@ public class Start
 	            	  jsonObj.put(columnName, value);
 	              }
 	          }   
-	          bulkRequest.add(client.prepareIndex("sub_order",
+	          bulkRequest.add(client.prepareIndex("sub_order_test",
 						"tbl_sub_order").setId(set.getString("SUB_ORDER_ID")).setSource(jsonObj.toString()));
 	      }
 	      System.out.println("线程"+Thread.currentThread().getName()+"处理了"+ss+"条数据");
@@ -119,8 +82,7 @@ public class Start
 		if (bulkResponse.hasFailures()) {
 			System.out.println(bulkResponse.buildFailureMessage());
 		}
-	  	  client.close();	
-	    
+	  	  
 	      set.close();
 	      stmt.close();
 	      conn.close();
--- src/main/java/com/gome/pop/ScanMysql2Es/Test.java
+++ src/main/java/com/gome/pop/ScanMysql2Es/Test.java
@@ -1,11 +1,18 @@
 package com.gome.pop.ScanMysql2Es;
 
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Test {
@@ -14,8 +21,18 @@ public class Test {
 	
 	public static void main(String[] args) throws Exception{
 		
-		
-		int count = Start.countSql(); //总数
+		 ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		 
+		 Client client =null;
+		 try {
+				client = TransportClient.builder().build()  
+			            .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.58.70.87"), 9300));//10.58.62.47 杨宇集群
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		 
+		int count = 20000; //总数
 		int page_size=1000; //每次10w条
 		int pages = count / page_size + 1; // 一共开多少个线程
 		
@@ -26,7 +43,7 @@ public class Test {
 		List<Runner> list=new ArrayList<Runner>();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 		
 		for(int i=1;i<=pages;i++){
-			Runner runner=new Runner("线程"+i,page_size,i,(i-1)*page_size);
+			Runner runner=new Runner(client,context,"线程"+i,page_size,i,(i-1)*page_size);
 			list.add(runner);
 		}
 		
@@ -41,19 +58,23 @@ public class Test {
 		private int page_size; //当前线程一次查询多少条记录
 		private int page;  //从第几页开始查询
 		private int start_index;//从第几条记录开始查询
+		private ApplicationContext context;
+		private Client client;
 		
-		public Runner(String threadName,int page_size,int page,int start_index){
+		public Runner(Client client,ApplicationContext context,String threadName,int page_size,int page,int start_index){
+			this.context = context;
 			this.page_size=page_size;
 			this.page=page;
 			this.threadName=threadName;
 			this.start_index=start_index;
+			this.client = client;
 		}
 		
 		public Long call() {
 			long startTime=new Date().getTime();
 			try {
 				//通过分页查询数据库
-				Start.creatJsonFromDB(start_index,page_size);
+				Start.creatJsonFromDB(start_index,page_size,context,client);
 				
 				System.out.println(threadName+"正处理从"+(page - 1) * page_size+"条开始的数据=============cost了"+(new Date().getTime()-startTime)/1000+"秒");
 			} catch (Exception e) {
